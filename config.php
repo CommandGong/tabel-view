467f7cbcbc2b09594788e055b271d4e8ff82c268
John Gong <yanbingong1990@gmail.com>
2020-11-30 14:19:11 -0500

    add common in css file

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
 $config['crossref_user_agent'] = "GroovyBib/1.1 (https://example.org/GroovyBib/; John:yanbingong1990@gmail.com) BasedOnFunkyLib/1.4";
-
+$config['crossref_doi'] = "10.2196/12121";
+//$config['crossref_doi'] = "10.1001/jama.290.1.57";
+//$config['crossref_doi'] = "10.1007/s10803-009-0825-1"; 
+//$config['crossref_doi'] = "10.1177/108835760301800201"; 
 
 $config_database['servername']="localhost";
 $config_database['username']="admin_john";
-$config_database['password']="admin123";
+$config_database['password']="";
 $config_database['db_name']="table_view";
 
 $config['home_url']="http://localhost/table-view";
